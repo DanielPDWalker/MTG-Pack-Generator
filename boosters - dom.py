69e799b533cf080d2f7a93f61b990424a4c3806b
DanielPDWalker <danielpdwalker@gmail.com>
2018-05-03 14:31:15 +0100

    Added: Typing out, Menu, Set choice, Pack number. Main()

--- boosters - dom.py
+++ boosters - dom.py
@@ -5,8 +5,17 @@ import os
 import sys
 import time
 
+sets = Set.all()
+sets_names = []
+selected_set = 'dom'
+number_of_packs = 0
+
+for i in sets:
+    sets_names.append(i.name.lower())
+
 # || User input and Interface ||
 # ||||||||||||||||||||||||||||||
+
 
 
 class KeyboardDisable():
@@ -48,6 +57,43 @@ def check_input(saved_input):
         return False
 
 
+def select_input_set(saved_input):
+    for i in sets:
+        if saved_input == i.name.lower():
+            return i.code
+
+
+def choose_set():
+    global selected_set
+    type_out("Please enter a set you would like a booster pack from:")
+    saved_input = input()
+    if saved_input.lower() in sets_names:
+        selected_set = select_input_set(saved_input.lower())
+    else:
+        type_out("Sorry but {} is not a set.".format(saved_input))
+        choose_set()
+
+
+def choose_quantity():
+    global number_of_packs
+    type_out("How many boosters would you like to generate?")
+    type_out("(Please note each booster takes roughly 15 seconds).")
+    saved_input = input()
+    try:
+        saved_input = int(saved_input)
+    except:
+        type_out("Please enter only whole numerical values.")
+        choose_quantity()
+
+
+def main():
+    """Runs the menu and set up"""
+    print("")
+    print("##################################################")
+    print("")
+    choose_set()
+    choose_quantity()
+
 
 # This sets the object to disable the keyboard. Used in type_out().
 disable_typing = KeyboardDisable()
@@ -55,6 +101,7 @@ disable_typing = KeyboardDisable()
 
 # || Generating Packs ||
 # ||||||||||||||||||||||
+
 
 def create_and_open(filename, mode):
     os.makedirs(current_pack, exist_ok=True)
@@ -70,14 +117,17 @@ def pack_number():
     return str(n) + '/'
 
 
-selected_set = 'dom'
 card = Set.generate_booster(selected_set)
 current_pack = pack_number()
+counter = 1
 
+def generate_pack():
 
-for i in card:
-    img_data = requests.get(
-        'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=' +
-        str(i.multiverse_id) + '&type=card').content
-    with create_and_open(current_pack + str(i.name) + '.jpg', 'wb') as f:
-        f.write(img_data)
+    for i in card:
+        img_data = requests.get(
+            'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=' +
+            str(i.multiverse_id) + '&type=card').content
+        with create_and_open(current_pack + str(counter) + ' ' + str(i.name) + '.jpg', 'wb') as f:
+            f.write(img_data)
+            counter += 1
+
